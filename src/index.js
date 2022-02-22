c3914a64dde4b902c229cac885f91b34bd632fe3
андрей пользователь <you@example.com>
2022-02-23 00:50:33 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 != 0) return false;
+  let arrStr = str.split("");
+  let result = [];
+  for (let i = 0; i < arrStr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (
+        arrStr[i] == bracketsConfig[j][1] &&
+        result[result.length - 1] == bracketsConfig[j][0]
+      ) {
+        result.pop();
+      } else if (arrStr[i] == bracketsConfig[j][0]) {
+        result.push(arrStr[i]);
+      }
+    }
+  }
+  if (result.length == 0) return true;
+  else return false;
+};
